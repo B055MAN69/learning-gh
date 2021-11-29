042404222b5f793f5cc285d00e3874bb58dc3ffc
B055MAN69 <tristanbutler669@gmail.com>
2021-11-29 13:01:37 -0600

    Initial Commit

---
+++ learningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learningGH
+//
+//  Created by PM Ada - Tristan Butler on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by PM Ada - Tristan Butler on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
